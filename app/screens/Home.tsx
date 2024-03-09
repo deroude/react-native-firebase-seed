8d6a9c1010e0f01a03e241056c9ad85a751d2186
Scarlat Delia <dalia2007@yahoo.com>
2024-03-09 12:22:02 +0200

    added the mp search bar and buttons(didnt do the styling part)

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,19 +1,62 @@
 import React, { useState } from "react";
+import MapView from 'react-native-maps';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainLayout from "./Layout";
 import { Paragraph } from "react-native-paper";
-
+import {SearchBar} from "@rneui/themed";
+import { View, Text, StyleSheet } from 'react-native';
+import { color } from "@rneui/themed/dist/config";
+export default function App(): React.JSX.Element {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
 const Stack = createNativeStackNavigator();
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
-const HomeScreen = ({ navigation }: any) => {
+
+type SearchBarComponentProps = {};
+
+const SwitchComponent: React.FunctionComponent<SearchBarComponentProps> = () => {
+const [search, setSearch] = useState("");
+
+const updateSearch = (search: React.SetStateAction<string>) => {
+  setSearch(search);
+};
+
+return (
+  <View style={style.view}>
+    <SearchBar
+      placeholder="Type Here..."
+      onChangeText={updateSearch}
+      value={search}
+      style={{ height: 30, width: 330 }}
+    />
+  </View>
+);
+};
+
+const style = StyleSheet.create({
+  view: {
+    margin: 10, 
+    width: 330,
+    color: '#FFFFF',
+  },
+  });
+  const HomeScreen = ({ navigation }: any) => {
 
     const [data, setData] = useState<{ label: string, quantity: number }[]>();
-//map page
-    return (
-        <MainLayout>
-            <Paragraph>Hello!</Paragraph>
-       </MainLayout>
-    );
-}
-
-export default HomeScreen;
+  //map page
+  } 
+  
+    
